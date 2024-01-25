99c9da3f5b8dbf50513c677d0f8bb49daf7b3d50
Faizan-Surani <suranifaizan52@gmail.com>
2024-01-25 20:38:44 +0530

    Add Pages and Component in the newsarticle app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import PrivateRouter from "./components/PrivateRouter";
+import Profile from "./pages/Profile";
 
 export default function App() {
   return (
@@ -10,7 +15,12 @@ export default function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route />
+          <Route path="/profile" element={<PrivateRouter />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
       </Router>
     </>
