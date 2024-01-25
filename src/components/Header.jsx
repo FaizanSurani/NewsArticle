6379047d0f9c67730c3938d17b04858024af4747
Faizan-Surani <suranifaizan52@gmail.com>
2024-01-25 23:20:22 +0530

    Complete Header Component and Create a SignIn Page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,44 @@
 import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export default function Header() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const pathMatchRoute = (route) => {
+    if (route === location.pathname) {
+      return true;
+    }
+  };
+
   return (
     <>
-      <div>
+      <div className="bg-white border-b shadow-sm sticky top-0 z-10">
         <header className="flex justify-between items-center px-3 mx-auto max-w-6xl bg-green-50">
           <div>
+            <img
+              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Frighttolife.org.uk%2Fwp-content%2Fuploads%2F2019%2F06%2FThe-Article-logo.png&f=1&nofb=1&ipt=2ef7e60060833db5d53e46b2c2137b890e18e290f9c55e8c1eb37100aa50f3db&ipo=images"
+              alt="News Article Logo"
+              className="h-11 cursor-pointer"
+              onClick={() => navigate("/")}
+            />
+          </div>
+          <div>
             <ul className="flex space-x-10">
-              <li className={`px-3 cursor-pointer`}>Home</li>
-              <li>Sign In</li>
+              <li
+                className={`py-3 cursor-pointer text-sm font-semibold text-gray-600 border-b-[3px] border-transparent 
+                ${pathMatchRoute("/") && "text-black border-b-blue-700"}`}
+                onClick={() => navigate("/")}>
+                Home
+              </li>
+              <li
+                className={`py-3 cursor-pointer text-sm font-semibold text-gray-600 border-b-[3px] border-transparent 
+                ${
+                  pathMatchRoute("/sign-in") && "text-black border-b-blue-700"
+                }`}
+                onClick={() => navigate("/sign-in")}>
+                Sign In
+              </li>
             </ul>
           </div>
         </header>
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,38 @@
 import React from "react";
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <>
+      <section>
+        <h1 className="text-center text-3xl font-bold mt-6">Sign In</h1>
+        <div className="flex justify-center items-center flex-wrap px-6 py-12 mx-auto max-w-6xl">
+          <div className="mb-12 md:w-[67%] lg:w-[50%] md:mb-6">
+            <img
+              className="w-full rounded-xl"
+              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.QH49PC552EETbMXBlVyU4wHaE7%26pid%3DApi&f=1&ipt=c3f3923f7b283b690263a40d8b706547624abd8de504553d24573f7890699faa&ipo=images"
+              alt="News Image"
+            />
+          </div>
+          <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+            <form>
+              <input
+                className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-blue-300 rounded transition ease-in-out"
+                type="email"
+                id="email"
+                placeholder="Email Address"
+              />
+              <div className="relative mb-6">
+                <input
+                  className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-blue-300 rounded transition ease-in-out"
+                  type="password"
+                  id="password"
+                  placeholder="Password"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
