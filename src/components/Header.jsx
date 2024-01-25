ba35bed3f2f4cac535e68bd3daad0b6d8ab3dacc
Faizan-Surani <suranifaizan52@gmail.com>
2024-01-25 22:42:27 +0530

    Add tailwindcss and change Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <div>
+        <header className="flex justify-between items-center px-3 mx-auto max-w-6xl bg-green-50">
+          <div>
+            <ul className="flex space-x-10">
+              <li className={`px-3 cursor-pointer`}>Home</li>
+              <li>Sign In</li>
+            </ul>
+          </div>
+        </header>
+      </div>
+    </>
+  );
 }
