1e9caa1ea45f11476d20ae2f7f86d5734b528066
Faizan-Surani <suranifaizan52@gmail.com>
2024-01-26 16:03:12 +0530

    Complete SignIn page and Add icons for Password Visibility

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoEye, IoEyeOff } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 export default function SignIn() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({ email: "", password: "" });
+
+  const { email, password } = formData;
+
+  const onInput = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
     <>
       <section>
@@ -16,18 +30,58 @@ export default function SignIn() {
           <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
             <form>
               <input
-                className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-blue-300 rounded transition ease-in-out"
+                className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-black rounded transition ease-in-out"
                 type="email"
                 id="email"
                 placeholder="Email Address"
+                value={email}
+                onChange={onInput}
               />
               <div className="relative mb-6">
                 <input
-                  className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-blue-300 rounded transition ease-in-out"
-                  type="password"
+                  className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-black rounded transition ease-in-out"
+                  type={showPassword ? "text" : "password"}
                   id="password"
                   placeholder="Password"
+                  value={password}
+                  onChange={onInput}
                 />
+                {showPassword ? (
+                  <IoEye
+                    className="absolute right-3 top-3 cursor-pointer text-xl"
+                    onClick={() => setShowPassword((prevState) => !prevState)}
+                  />
+                ) : (
+                  <IoEyeOff
+                    className="absolute right-3 top-3 text-xl cursor-pointer"
+                    onClick={() => setShowPassword((prevState) => !prevState)}
+                  />
+                )}
+              </div>
+              <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+                <p className="mb-6">
+                  Don't have an Account?{" "}
+                  <Link
+                    to="/sign-up"
+                    className="text-red-600 hover:text-red-700 ml-1 transition duration-200 ease-in-out">
+                    Register
+                  </Link>
+                </p>
+                <p>
+                  <Link
+                    to="/forgot-password"
+                    className="text-blue-600 hover:text-blue-700 ml-1 transition duration-200 ease-in-out">
+                    Forgot Password?
+                  </Link>
+                </p>
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-sm hover:bg-blue-700 hover:shadow-sm transition duration-150 ease-in-out">
+                Sign In
+              </button>
+              <div className="my-4 before:border-t flex before:flex-1 items-center before:border-gray-300 after:border-gray-300 after:border-t after:flex-1">
+                <p className="text-center font-semibold mx-4">OR</p>
               </div>
             </form>
           </div>
