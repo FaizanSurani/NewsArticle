287e88eed3c79a2f97145014d4e7912129eb2683
Faizan-Surani <suranifaizan52@gmail.com>
2024-01-28 14:46:41 +0530

    Complete the Profile Page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
+import { getAuth, updateProfile } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../Firebase";
 
 export default function Profile() {
-  return <div>Profile</div>;
+  const auth = getAuth();
+  const navigate = useNavigate();
+
+  const [changeDetail, setChangeDetail] = useState(false);
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  const { name, email } = formData;
+
+  const onLogout = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        await updateProfile(auth.currentUser, { displayName: name });
+
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name: name,
+        });
+      }
+      toast.success("Profile Details Updated!");
+    } catch (error) {
+      toast.error("Could not Update the Profile Details");
+    }
+  };
+
+  return (
+    <>
+      <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
+        <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
+        <div className="w-full md:w-[50%] mt-6 px-3">
+          <form>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              disabled={!changeDetail}
+              onChange={onChange}
+              className={`mb-6 px-4 py-2 w-full text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
+                changeDetail && "bg-red-200 focus:bg-red-200"
+              }`}
+            />
+            <input
+              type="email"
+              id="email"
+              value={email}
+              disabled={!changeDetail}
+              onChange={onChange}
+              className="px-4 py-2 mb-6 w-full text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+            />
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
+              <p className="flex items-center">
+                Do you want to change your name?
+                <span
+                  onClick={() => {
+                    changeDetail && onSubmit();
+                    setChangeDetail((prevState) => !prevState);
+                  }}
+                  className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">
+                  {changeDetail ? "Apply Changes" : "Edit"}
+                </span>
+              </p>
+              <p
+                onClick={onLogout}
+                className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer">
+                Sign Out
+              </p>
+            </div>
+          </form>
+          {/* <button
+            type="submit"
+            className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md transition ease-in-out duration-150 hover:bg-blue-700 hover:shadow-lg active:bg-blue-800">
+            <Link
+              to="/create-listing"
+              className="flex justify-center items-center">
+              Sell or Rent your Home
+            </Link>
+          </button> */}
+        </div>
+      </section>
+    </>
+  );
 }
