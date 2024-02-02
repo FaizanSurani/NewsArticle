fe65dacc0e3cb041a9456210b56837e9f25e7375
Faizan-Surani <suranifaizan52@gmail.com>
2024-02-02 23:54:05 +0530

    Add newsInfo page in the project

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -10,7 +10,7 @@ export default function Results({ newsData }) {
       <div className="grid grid-cols-2 md:grid-cols-2">
         {newsData.map((data, index) => (
           <ul className="border border-gray-600 shadow-lg mb-6 rounded w-full px-7 py-3 hover:shadow-xl">
-            <Link to="/newsInfo/:id">
+            <Link to={`/newsInfo/${data.source.id}`}>
               <h3 className="mb-4 font-bold text-2xl">{`Author : ${data.author}`}</h3>
               <li
                 key={index}
--- src/pages/NewsInfo.jsx
+++ src/pages/NewsInfo.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function () {
+  const { id } = useParams();
+  const [newsData, setNewsData] = useState();
+
+  useEffect(() => {}, []);
+
   return (
     <>
       <div></div>
