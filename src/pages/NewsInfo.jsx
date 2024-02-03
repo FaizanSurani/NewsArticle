0933fcf5020ffd0e616b585ad0ae3cec07cc3b83
Faizan-Surani <suranifaizan52@gmail.com>
2024-02-03 12:07:03 +0530

    Create newsInfo Page to add news details on the page and update the state

--- src/pages/NewsInfo.jsx
+++ src/pages/NewsInfo.jsx
@@ -1,15 +1,48 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export default function () {
   const { id } = useParams();
-  const [newsData, setNewsData] = useState();
+  const [newsData, setNewsData] = useState({});
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      if (id) {
+        try {
+          const res = await fetch(
+            `https://newsapi.org/v2/everything?q=${id}&apiKey=470a464d3e6442a2a6c3d847be2d0ee9`
+          );
+          const data = await res.json();
+
+          if (data.status === "ok") {
+            const foundNews = data.articles.find((news) => news.title === id);
+
+            if (foundNews) {
+              setNewsData(foundNews);
+            }
+          }
+        } catch (error) {
+          toast.error("Couldn't Fetch the data!");
+          console.log(error);
+        }
+      }
+    };
+    fetchData();
+  }, [id]);
 
   return (
     <>
-      <div></div>
+      <div>
+        {/* {newsData && (
+          <ul>
+            {newsData.map((news) => (
+              <li>{news.title}</li>
+            ))}
+          </ul>
+        )} */}
+        {newsData.title}
+      </div>
     </>
   );
 }
