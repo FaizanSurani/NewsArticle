c616d04a4015b3b84629d7550842a868f4cee90c
Faizan-Surani <suranifaizan52@gmail.com>
2024-02-13 12:58:39 +0530

    Add key and set image for the user

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -7,19 +7,21 @@ export default function Results({ newsData }) {
       <div className="grid grid-cols-2 md:grid-cols-2 text-center px-7 py-3 my-3 mx-3">
         {newsData?.map((data, index) => (
           <div className="border border-gray-600 shadow-lg mb-6 rounded w-full hover:shadow-xl bg-gray-900 text-white">
-            <img
-              src={data.urlToImage}
-              alt=""
-              style={{ height: "200px", width: "360px" }}
-              className="rounded mb-6 border border-white"
-            />
+            <div className="flex justify-center items-center mt-4">
+              <img
+                src={data.urlToImage}
+                alt="..."
+                style={{ height: "200px", width: "360px" }}
+                className="rounded mb-6 border border-white"
+              />
+            </div>
             <div>
               <h3 className="mb-4 font-bold text-2xl">{`Author : ${data.author}`}</h3>
               <div className="mb-2 font-serif flex items-center justify-center text-xl">
                 {data.title}
               </div>
               <a href={data.url} target="_blank" rel="noopener noreferrer">
-                <button className="bg-blue-600 px-7 py-3 rounded text-white hover:bg-blue-700 active:to-blue-800 transition duration-150 ease-in-out uppercase mt-3">
+                <button className="bg-blue-600 px-7 py-3 rounded text-white hover:bg-blue-700 active:to-blue-800 transition duration-150 ease-in-out uppercase mt-3 mb-3">
                   Read More
                 </button>
               </a>
