d95555e4fa4185684db0ca62e9fc809ac41f2c10
Faizan-Surani <suranifaizan52@gmail.com>
2024-02-16 18:10:23 +0530

    Complete the MyPreference component

--- src/components/MyPreference.jsx
+++ src/components/MyPreference.jsx
@@ -1,22 +1,34 @@
-import React, { useState } from "react";
-
+import image from "../assets/news.jpg";
 export default function MyPreference({ title, description, src, url }) {
   return (
     <>
-      <div className="" style={{ maxWidth: "345px", maxHeight: "500px" }}>
-        <div className="mt-6 mb-6 border border-gray-300 bg-gray-900 text-white px-7 py-3">
-          <img src={src} alt="..." />
-          <div>
-            <h5>{title}</h5>
-            <p>{description}</p>
-            <a href={url} target="_blank" rel="noopener noreferrer">
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        <div style={{ maxWidth: "345px", maxHeight: "500px" }}>
+          <div className="mt-6 mb-6 border border-gray-300 bg-gray-900 text-white px-7 py-3">
+            <img
+              src={src ? src : image}
+              alt="..."
+              className="rounded mt-3 mb-3"
+              style={{ maxWidth: "200px", maxHeight: "200px" }}
+            />
+            <div>
+              <h5 className="font-bold text-xl mb-3 mt-3">
+                {title
+                  ? title.slice(0, 30)
+                  : "Title is not provided by the author!"}
+              </h5>
+              <p>
+                {description
+                  ? description.slice(0, 50)
+                  : "News Description is not provided by the Author!"}
+              </p>
               <button className="mt-3 mb-3 bg-blue-600 px-7 py-3 uppercase rounded text-center">
                 Read More
               </button>
-            </a>
+            </div>
           </div>
         </div>
-      </div>
+      </a>
     </>
   );
 }
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,23 +1,26 @@
 import React from "react";
+import image from "../assets/news.jpg";
 
 export default function Results({ newsData }) {
   return (
     <>
-      <div className="grid grid-cols-2 md:grid-cols-2 text-center px-7 py-3 my-3 mx-3">
+      <div className="sm:grid sm:grid-cols-2 md:grid-cols-3 text-center px-7 py-3">
         {newsData?.map((data, index) => (
           <div
             key={index}
             className="border border-gray-600 shadow-lg mb-6 rounded w-full hover:shadow-xl bg-gray-900 text-white">
             <div className="flex justify-center items-center mt-4">
               <img
-                src={data.urlToImage}
+                src={data.urlToImage ? data.urlToImage : image}
                 alt="..."
                 style={{ height: "200px", width: "360px" }}
                 className="rounded mb-6 border border-white"
               />
             </div>
             <div>
-              <h3 className="mb-4 font-bold text-2xl">{`Author : ${data.author}`}</h3>
+              <h3 className="mb-4 font-bold text-2xl">{`Author : ${
+                data.author ? data.author : "Unknown"
+              }`}</h3>
               <div className="mb-2 font-serif flex items-center justify-center text-xl">
                 {data.title}
               </div>
